bcad927a0cd373d7b2a21ea00264b513265185b9
Matthew O <deltas2k@gmail.com>
2019-10-03 13:53:11 -0600

    model and controller commit

---
+++ representitives/representitives/States.swift
@@ -0,0 +1,13 @@
+//
+//  States.swift
+//  representitives
+//
+//  Created by Matthew O'Connor on 10/3/19.
+//  Copyright © 2019 Matthew O'Connor. All rights reserved.
+//
+
+import Foundation
+
+enum States {
+    static var all = ["AK", "AL", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
+}
