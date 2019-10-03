456693523bcc4fab759baa4d106b69633a3aa7b8
Matthew O <deltas2k@gmail.com>
2019-10-03 15:10:18 -0600

    finish commit

---
+++ representitives/representitives/RepTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RepTableViewCell.swift
+//  representitives
+//
+//  Created by Matthew O'Connor on 10/3/19.
+//  Copyright © 2019 Matthew O'Connor. All rights reserved.
+//
+
+import UIKit
+
+class RepTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
